73c28e1267b5811d6bd9449dff89785774551946
zxh0 <zhangxiuhong2013@gmail.com>
2020-01-14 14:43:30 +0800

    Update debug API

--- src/debug.rs
+++ src/debug.rs
@@ -2,6 +2,18 @@ use super::native;
 
 pub fn println(msg: &str) {
     unsafe {
-        native::sci_print(msg.as_ptr(), msg.len() as i32);
+        native::sci_print_str(msg.as_ptr(), msg.len() as i32);
+    }
+}
+
+pub fn print_i32(i: i32) {
+    unsafe {
+        native::sci_print_int32(i);
+    }
+}
+
+pub fn print_i64(i: i64) {
+    unsafe {
+        native::sci_print_int64(i);
     }
 }
