f1488125faa92acb499a642a8208d28f4f8f4fac
zxh0 <zhangxiuhong2013@gmail.com>
2020-01-15 16:04:54 +0800

    Add multisig example

--- examples/erc721/main.rs
+++ examples/erc721/main.rs
@@ -1,3 +1,5 @@
+// https://github.com/AYIDouble/Simple-Game-ERC-721-Token-Template
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ examples/multisig/main.rs
@@ -0,0 +1,6 @@
+// https://medium.com/@ChrisLundkvist/exploring-simpler-ethereum-multisig-contracts-b71020c19037
+// https://github.com/christianlundkvist/simple-multisig/blob/master/contracts/SimpleMultiSig.sol
+
+fn main() {
+    println!("Hello, world!");
+}
