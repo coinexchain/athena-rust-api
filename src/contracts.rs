05b437acd63531b440852c06ec95c016975ae57b
zxh0 <zhangxiuhong2013@gmail.com>
2020-02-14 11:22:33 +0800

    Update contracts.rs

--- src/contracts.rs
+++ src/contracts.rs
@@ -1,6 +1,29 @@
 use super::{native, HostData, HostStr};
 
 /*
+    pub fn sci_query_module(
+        route: RawPtr,
+        route_len: i32,
+        param: RawPtr,
+        param_len: i32,
+        res_len_ptr: I32Ptr,
+    ) -> RawPtr;
+*/
+pub fn query_module(route: &str, params: &[u8]) {
+    unsafe {
+        let mut len: i32 = 0;
+        let ptr = native::sci_query_module(
+            route.as_ptr(),
+            route.len() as i32,
+            params.as_ptr(),
+            params.len() as i32,
+            &mut len,
+        );
+        // TODO
+    }
+}
+
+/*
     pub fn sci_call_contract(
         id: i64,
         route: RawPtr,
@@ -24,6 +47,33 @@ pub fn call(id: i64, route: &str, params: &[u8]) {
             &mut ret,
             &mut len,
         );
+        // TODO
+    }
+}
+
+// pub fn sci_prepare_coins(denom_ptr: RawPtr, denom_len: i32, amount: i64);
+pub fn prepare_coins(denom: &str, amt: i64) {
+    unsafe { native::sci_prepare_coins(denom.as_ptr(), denom.len() as i32, amt) }
+}
+
+pub fn msg2run_append(msg_type: &[u8], msg_json: &[u8]) {
+    unsafe {
+        native::sci_messagestorun_append(
+            msg_type.as_ptr(),
+            msg_type.len() as i32,
+            msg_json.as_ptr(),
+            msg_json.len() as i32,
+        )
+    }
+}
+pub fn msg2run_len() -> i32 {
+    unsafe { native::sci_messagestorun_len() }
+}
+// pub fn sci_messagestorun_get(n: i32, len_ptr: I32Ptr) -> RawPtr;
+pub fn msg2run_get(n: i32) {
+    unsafe {
+        let mut len: i32 = 0;
+        let ptr = native::sci_messagestorun_get(n, &mut len);
         // TODO
     }
 }
